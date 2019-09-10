a32d43f844dd736c967ed2bf476838bb36131c6a
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-10 18:08:19 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,7 +24,7 @@ class RecipesController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(
       :title,
-      ingredient_attributes: [
+      ingredients_attributes: [
         :name,
         :quantity
       ]
